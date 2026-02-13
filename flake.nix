f5600dce32ce20911b47b7c36ef62d9eacc0e94e
Patrick Jeremic <pjeremic@chorus.one>
2026-02-13 11:27:22 +0100

    Change to the uv2nix template

--- flake.nix
+++ flake.nix
@@ -4,69 +4,126 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
     }:
+    let
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      projectOverlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      editableOverlay = workspace.mkEditablePyprojectOverlay {
+        root = "$REPO_ROOT";
+      };
+    in
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python313.override {
-          packageOverrides = pyFinal: _pyPrev: {
-            opencv4 = pkgs.opencv4.override {
-              enablePython = true;
-              pythonPackages = pyFinal;
-              enableGtk3 = true;
-              gtk3 = pkgs.gtk3;
-            };
-          };
+        python = pkgs.python313;
+        runtimeLibs = with pkgs; [
+          glib
+          libGL
+          stdenv.cc.cc.lib
+          xorg.libxcb
+          zbar
+          zlib
+
+          # Required for the uv package opencv wheel
+          fontconfig
+          dejavu_fonts
+        ];
+
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              pkgs.lib.composeManyExtensions [
+                pyproject-build-systems.overlays.wheel
+                projectOverlay
+              ]
+            );
+
+        editablePythonSet = pythonSet.overrideScope editableOverlay;
+
+        appVenv = pythonSet.mkVirtualEnv "xfer-env" workspace.deps.default;
+        devVenv = editablePythonSet.mkVirtualEnv "xfer-dev-env" workspace.deps.all;
+        appWrapped = pkgs.symlinkJoin {
+          name = "xfer";
+          paths = [ appVenv ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/xfer \
+              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath runtimeLibs}
+          '';
         };
-        pythonEnv = python.withPackages (ps: [
-          ps.click
-          ps.imageio
-          ps.numpy
-          ps."opencv-python"
-          ps.pillow
-          ps.pyzbar
-          ps.qrcode
-        ]);
-        xfer-package = python.pkgs.buildPythonApplication {
-          pname = "xfer";
-          version = "0.0.5";
-          format = "pyproject";
-          src = self;
-
-          nativeBuildInputs = with python.pkgs; [
-            setuptools
-            wheel
-          ];
-          propagatedBuildInputs = [
-            python.pkgs.click
-            python.pkgs.imageio
-            python.pkgs.numpy
-            python.pkgs."opencv-python"
-            python.pkgs.pillow
-            python.pkgs.pyzbar
-            python.pkgs.qrcode
-          ];
-
-          pythonImportsCheck = [ "xfer" ];
-        };
-
       in
       {
-        packages.default = xfer-package;
-        apps.default = flake-utils.lib.mkApp { drv = xfer-package; };
+        packages.default = appWrapped;
+        apps.default = flake-utils.lib.mkApp {
+          drv = appWrapped;
+          exePath = "/bin/xfer";
+        };
 
         devShells.default = pkgs.mkShell {
           packages = [
-            pythonEnv
-          ];
+            devVenv
+            pkgs.uv
+          ]
+          ++ runtimeLibs;
+
+          UV_NO_SYNC = "1";
+          UV_PYTHON = editablePythonSet.python.interpreter;
+          UV_PYTHON_DOWNLOADS = "never";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+          NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+          NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+          QT_QPA_FONTDIR = "${pkgs.dejavu_fonts}/share/fonts/truetype";
+          XFER_QT_FONTDIR = "${pkgs.dejavu_fonts}/share/fonts/truetype";
+          QT_QPA_PLATFORM = "xcb";
+          FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
+          FONTCONFIG_PATH = "${pkgs.fontconfig.out}/etc/fonts";
+
+          shellHook = ''
+            unset PYTHONPATH
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
+            export QT_QPA_FONTDIR="${pkgs.dejavu_fonts}/share/fonts/truetype"
+            export XFER_QT_FONTDIR="${pkgs.dejavu_fonts}/share/fonts/truetype"
+            export QT_QPA_PLATFORM="xcb"
+            export FONTCONFIG_FILE="${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
+            export FONTCONFIG_PATH="${pkgs.fontconfig.out}/etc/fonts"
+            unset QT_STYLE_OVERRIDE
+          '';
         };
       }
     );
